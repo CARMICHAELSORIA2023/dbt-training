bcb4a8d0afb16bdc89be4135ad28849c402589c9
soriac@ph.ibm.com <soriac@ph.ibm.com>
2023-09-07 06:10:29 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+SELECT
+-- from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+FROM {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
