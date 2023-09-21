274a2278b81bb530e8717a7103085a31ed272c31
Carmichael Soria <soriac@ph.ibm.com>
2023-09-21 01:51:01 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -16,9 +16,11 @@ c.country,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup
 FROM {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+{{limit_data_in_dev('orderdate')}}
